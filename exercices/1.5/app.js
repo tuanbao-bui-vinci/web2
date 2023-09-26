cdbd1932ed67399b53d84c6eaca3af1c0ec7173b
Tuan Bao Bui <tuanbao.bui@student.vinci.be>
2023-09-26 09:24:13 +0200

    ex 1.5 finished + creation ex 1.6

--- exercices/1.5/app.js
+++ exercices/1.5/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var filmsRouter = require('./routes/films');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -13,19 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    const currentOperation = `${req.method} ${req.path}`;
-    const currentOperationCounter = stats[currentOperation];
-    if (currentOperationCounter === undefined) stats[currentOperation] = 0;
-    stats[currentOperation] += 1;
-    const statsMessage = `Request counter : \n${Object.keys(stats)
-      .map((operation) => `- ${operation} : ${stats[operation]}`)
-      .join('\n')}
-        `;
-    console.log(statsMessage);
-    next();
-  });
-  
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ exercices/1.5/routes/films.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+
+const FILM = [
+    {
+      id: 1,
+      title: 'Harry Potter et la chambres des secrets',
+      duration: 130,
+      budget: 300,
+      link: 'imdb'
+    },
+    {
+      id: 2,
+      title: 'Hobbit',
+      duration: 120,
+      budget: 200,
+      link: 'imdb'
+    },
+    {
+      id: 3,
+      title: 'Avengers: Endgame',
+      duration: 140,
+      budget: 400,
+      link: 'imdb'
+    },
+  ];
+  
+
+  router.get('/', (req, res) => {
+    const minFilmDuration = req?.query ? parseInt(req.query['minimum-duration']) 
+    : undefined;
+
+    if( typeof minFilmDuration !=='number' || minFilmDuration<=0 )
+    return res.sendStatus(400);
+
+    if(!minFilmDuration) return res.json(FILM);
+
+    const filmsReachingMinimumDuration = FILM.filter((film)=> film.duration >= minFilmDuration);
+    
+    return res.json(filmsReachingMinimumDuration);
+  });
+
+  router.get('/:id',(req,res)=>{
+    const indexOfFilm = FILM.findIndex((films)=>films.id == req.params.id);
+
+    if(indexOfFilm<0) return res.sendStatus(404); 
+    
+    return res.json(FILM[indexOfFilm]);
+  });
+
+  router.post('/',(req, res) =>{
+    const title = req?.body?.title?.trim()?.length !== 0 ? req.body.title : undefined;
+  const link = req?.body?.content?.trim().length !== 0 ? req.body.link : undefined;
+  const duration =
+    typeof req?.body?.duration !== 'number' || req.body.duration < 0
+      ? undefined
+      : req.body.duration;
+  const budget =
+    typeof req?.body?.budget !== 'number' || req.body.budget < 0
+      ? undefined
+      : req.body.budget;
+
+  if (!title || !link || !duration || !budget) return res.sendStatus(400);
+
+  const lastItemIndex = FILM?.length !== 0 ? FILM.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? FILM[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  const newFilm = { id: nextId, title, link, duration, budget };
+
+  FILM.push(newFilm);
+
+  return res.json(newFilm);
+  });
+
+
+
+module.exports = router;
