5a90bb00beef8fd608680a040ca9464925c8610f
Tuan Bao Bui <tuanbao.bui@student.vinci.be>
2023-10-09 15:06:31 +0200

    2.2 : HMI : dom interaction.

---
+++ exercices/2.1/index.js
@@ -0,0 +1,8 @@
+const dateTimeNow = new Date();
+alert(dateTimeNow + ": This is the best moment to have a look at this website !");
+
+
+console.log(dateTimeNow.toLocaleDateString()); // 17/08/2020
+console.log(dateTimeNow.toLocaleTimeString()); // 13:26:15
+
+
---
+++ exercices/2.2/index.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelector("#click1");
+const count = document.querySelector("#click2");
+
+let cpt = 0;
+
+
+window.addEventListener("click", () => {
+      cpt++;
+      
+      buttons.innerText = "Hello world!";
+      count.innerText = "Vous avez cliqué: "+ cpt;
+      if(cpt >=5){
+        buttons.innerText = "Bravo, bel échauffement !";
+        count.innerText = "Vous avez cliqué: "+ cpt;
+      };
+      if(cpt >= 10){
+        buttons.innerText = "Vous êtes passé maître en l'art du clic !"
+        count.innerText = "Vous avez cliqué: "+ cpt;
+      };
+      
+});
+
+
